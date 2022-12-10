44ccd16f756dc23e4b9a354b735dfac89effda3c
rnc-personal <richard.challenger@protonmail.com>
2022-12-10 13:42:12 +0000

    Initial Commit, add simple term menu library

--- run.py
+++ run.py
@@ -1,3 +1,20 @@
 # Your code goes here.
 # You can delete these comments, but do not change the name of this file
 # Write your code to expect a terminal of 80 characters wide and 24 rows high
+from simple_term_menu import TerminalMenu
+
+
+
+
+
+def main():
+    """
+    Main game Loop
+    """
+    options = ["Warrior", "Mage", "Rogue"]
+    terminal_menu = TerminalMenu(options)
+    menu_entry_index = terminal_menu.show()
+    print(f"You have selected {options[menu_entry_index]}!")
+
+if __name__ == "__main__":
+    main()
