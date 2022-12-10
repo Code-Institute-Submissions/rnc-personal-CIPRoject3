bfa81b083acf90eef42a7af120a6438a38149c8a
rnc-personal <richard.challenger@protonmail.com>
2022-12-10 16:21:27 +0000

    add dice rolls + todos for them

--- run.py
+++ run.py
@@ -2,6 +2,7 @@
 # You can delete these comments, but do not change the name of this file
 # Write your code to expect a terminal of 80 characters wide and 24 rows high
 from simple_term_menu import TerminalMenu
+from random import randint
 
 # Global Information about the player
 PLAYER_HP = 25
@@ -39,6 +40,41 @@ def player_class_selection(player_class):
 
     return player_class
 
+# Dice Rolls
+# Could this be one function with paramenters for the rarity?
+def low_weighted_dice_roll():
+    """
+    Dice roll with a weighting of 1/6
+    """
+    roll_6 = randint(1, 6)
+    print(f"You scored a {roll_6 }")
+    return roll_6
+
+def med_weighted_dice_roll():
+    """
+    Dice roll with a weighting of 1/24
+    """
+    roll_24 = randint(1, 24)
+    print(f"You scored a {roll_24 }")
+    return roll_24
+
+def high_weighted_dice_roll():
+    """
+    Dice roll with a weighting of 1/50
+    """
+    roll_50 = randint(1, 50)
+    print(f"You scored a {roll_50 }")
+    return roll_50
+
+def legendary_weighted_dice_roll():
+    """
+    Dice roll with a weighting of 1/100
+    """
+    roll_100 = randint(1, 100)
+    print(f"You scored a {roll_100 }")
+    return roll_100
+
+# Player Navigation
 def player_nav():
     """
     Allows the users to choose where to move next
@@ -63,13 +99,13 @@ def player_attack(dmg):
     global MONSTER_HP
     MONSTER_HP = MONSTER_HP - dmg
     print(f"The Monsters HP is: {MONSTER_HP}")
-    #If Statement about the Monsters Condition (is it dead?)
+    # If Statement about the Monsters Condition (is it dead?)
     # Is there a terminal library for colored terminal text?
     return MONSTER_HP
 
 
 
-
+# ------------------MAIN GAME LOOP------------------
 def main():
     """
     Main game Loop
@@ -83,6 +119,7 @@ def main():
     # monster_attack(3)
     # player_attack(PLAYER_DMG)
     player_nav()
+    # legendary_weighted_dice_roll()
   
 
 if __name__ == "__main__":
