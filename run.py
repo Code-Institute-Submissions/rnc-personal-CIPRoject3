34ca7f51820698ca76737327f67e3f20bc0532b7
rnc-personal <richard.challenger@protonmail.com>
2022-12-11 15:29:55 +0000

    add TODO Cell names

--- run.py
+++ run.py
@@ -21,6 +21,7 @@ PLAYER_ENCOUNTER = False
 MONSTER_HP = randint(5, 40)
 MONSTER_DMG = randint(1, 5)
 
+# TODO - Each Cell Should have a name, think about how do do this: K/V Dictionary?
 MAP_GRID = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11,
 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30,
 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49,
